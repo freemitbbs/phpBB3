2f4d3e5b0e0b327c5d61f36bd511e0bcc2de592a
Huahai Yang <huahai.yang@gmail.com>
2026-06-11 18:00:54 -0700

    fix collapsible button

--- ext/phpbb/collapsiblecategories/styles/all/template/js/collapsiblecategories.js
+++ ext/phpbb/collapsiblecategories/styles/all/template/js/collapsiblecategories.js
@@ -2,14 +2,117 @@
 
 	'use strict';
 
+	var storagePrefix = 'phpbb_collapsiblecategories_';
+
 	// Get the collapsible element (has class .topiclist.forums OR .collapsible)
 	$.fn.getCollapsible = function() {
 		return this.closest('.forabg').find('.topiclist.forums, .collapsible').eq(0);
 	};
 
+	function isHiddenValue(value) {
+		return value === true || value === '1' || value === 'true';
+	}
+
+	function getCollapseId($button) {
+		var href = $button.attr('href') || '',
+			match = href.match(/\/collapse\/([^?&#/]+)/);
+
+		return match ? decodeURIComponent(match[1]) : '';
+	}
+
+	function getStoredHidden(collapseId) {
+		if (!collapseId || !window.localStorage) {
+			return null;
+		}
+
+		try {
+			var value = window.localStorage.getItem(storagePrefix + collapseId);
+			return value === null ? null : value === '1';
+		} catch (e) {
+			return null;
+		}
+	}
+
+	function setStoredHidden(collapseId, hidden) {
+		if (!collapseId || !window.localStorage) {
+			return;
+		}
+
+		try {
+			window.localStorage.setItem(storagePrefix + collapseId, hidden ? '1' : '0');
+		} catch (e) {}
+	}
+
+	function swapButtonState($button) {
+		var oldTitle = $button.attr('title'),
+			newTitle = $button.attr('data-title-alt');
+
+		$button
+			.attr({
+				'title': newTitle,
+				'data-title-alt': oldTitle
+			})
+			.find('i')
+			.toggleClass('fa-plus-square fa-minus-square');
+	}
+
+	function setCollapseState($button, hidden, animate) {
+		var currentHidden = isHiddenValue($button.attr('data-collapse-hidden')),
+			$content = $button.getCollapsible();
+
+		if (!$content.length) {
+			return;
+		}
+
+		if (currentHidden !== hidden) {
+			swapButtonState($button);
+		}
+
+		$button.attr('data-collapse-hidden', hidden ? '1' : '0');
+		$content.stop(true, true);
+		if (animate) {
+			hidden ? $content.slideUp('fast') : $content.slideDown('fast');
+		} else {
+			hidden ? $content.hide() : $content.show();
+		}
+	}
+
+	function persistCollapseState($button) {
+		var href = ($button.attr('href') || '').replace(/&amp;/g, '&');
+		if (!href) {
+			return;
+		}
+
+		$.ajax({
+			url: href,
+			type: 'GET',
+			cache: false,
+			global: false
+		}).fail($.noop);
+	}
+
+	function handleCollapseClick(button, event) {
+		var $button = $(button),
+			collapseId = getCollapseId($button),
+			hidden = !isHiddenValue($button.attr('data-collapse-hidden'));
+
+		event.preventDefault();
+		event.stopPropagation();
+		if (event.stopImmediatePropagation) {
+			event.stopImmediatePropagation();
+		}
+
+		setCollapseState($button, hidden, true);
+		setStoredHidden(collapseId, hidden);
+		persistCollapseState($button);
+	}
+
 	$('a.collapse-btn').each(function() {
 		var $this = $(this),
-			hidden = $this.attr('data-hidden'),
+			collapseId = getCollapseId($this),
+			storedHidden = getStoredHidden(collapseId),
+			initialHidden = isHiddenValue($this.attr('data-hidden')),
+			hidden = storedHidden === null ? initialHidden : storedHidden,
 			$content = $this.getCollapsible();
 
 		// Return if no collapsible content could be found
@@ -18,31 +121,27 @@
 		}
 
 		// Unhide the collapse buttons (makes them JS dependent)
-		$this.show();
+		$this
+			.attr({
+				'data-ajax': 'false',
+				'data-collapse-hidden': initialHidden ? '1' : '0'
+			})
+			.show();
 
-		// Hide hidden forums on load
-		if (hidden) {
-			$content.hide();
-		}
+		setCollapseState($this, hidden, false);
 	});
 
 	phpbb.addAjaxCallback('phpbb_collapse', function(res) {
 		if (res.success) {
-			var oldTitle = $(this).attr('title'),
-				newTitle = $(this).attr('data-title-alt');
-			$(this)
-				.attr({
-					'title': newTitle,
-					'data-title-alt': oldTitle
-				})
-				.find('i')
-				.toggleClass('fa-plus-square fa-minus-square')
-				.end()
-				.getCollapsible()
-				.stop(true, true)
-				.slideToggle('fast')
-			;
+			setCollapseState($(this), !isHiddenValue($(this).attr('data-collapse-hidden')), true);
 		}
 	});
+
+	document.addEventListener('click', function(event) {
+		var button = $(event.target).closest('a.collapse-btn')[0];
+		if (button) {
+			handleCollapseClick(button, event);
+		}
+	}, true);
 
 })(jQuery); // Avoid conflicts with other libraries
